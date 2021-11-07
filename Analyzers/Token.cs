7fb69b5833a44987937a678202d69e337f4b21f0
Dev <gekomixy@gmail.com>
2021-11-07 14:25:14 +0300

    Remove unnecessary fields

--- Analyzers/Token.cs
+++ Analyzers/Token.cs
@@ -15,28 +15,13 @@ namespace Lang.Analyzers
 
     public class Token
     {
-        private readonly int _number;
-        private readonly TokenType _type;
+        public int Number { get; private set; }
+        public TokenType Type { get; private set; }
 
         public Token(TokenType type, int number = 0)
         {
-            _number = number;
-            _type = type;
-        }
-
-        public int Number
-        {
-            get
-            {
-                return _number;
-            } 
-        }
-        public TokenType Type
-        {
-            get
-            {
-                return _type;
-            } 
+            Number = number;
+            Type = type;
         }
     }
 }
