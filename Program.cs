f7b1bd56c49c6bc1e63821933cd6ab064c1dbab7
Dev <gekomixy@gmail.com>
2021-11-07 14:44:57 +0300

    Upgrade user interface

--- Program.cs
+++ Program.cs
@@ -7,10 +7,15 @@ namespace Lang
     {
         static void Main(string[] args)
         {
-            var lexer = new Lexer("(3-4)*5l");
-            var lexems = lexer.Lex();
-            foreach(Token lexem in lexems)
-                Console.Write($"{lexem.Type}: {lexem.Number} ");
+            var expression = Console.ReadLine();
+            var lexems = new Lexer(expression).Lex();
+            foreach(Token token in lexems)
+            {
+                Console.Write($" {token.Type}");
+                if(token.Type == TokenType.Number)
+                    Console.Write($"({token.Number})");
+            }
+
         }
     }
 }
